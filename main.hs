55a4f489ee9928304691859bdd2250061052767a
Mjturn <Mjturn@outlook.com>
2024-06-18 19:31:19 -0400

    create Pomodoro Timer

---
+++ main.hs
@@ -0,0 +1,35 @@
+import Text.Printf (printf)
+import System.IO (hFlush, stdout)
+import Control.Concurrent (threadDelay)
+
+printTime :: Int -> IO ()
+printTime totalSeconds = do
+    let minutes = totalSeconds `div` 60
+        seconds = totalSeconds `mod` 60
+    printf "\r%02d:%02d" minutes seconds
+    hFlush stdout
+
+countDown :: Int -> IO ()
+countDown 0 = putStrLn "\nTime's up!"
+countDown time = do
+    printTime time
+    threadDelay 1000000
+    countDown (time - 1)
+
+pomodoro :: Int -> Int -> IO ()
+pomodoro workMinutes breakMinutes = do
+    putStrLn "Time to get to work!"
+    countDown (workMinutes * 60)
+    putStrLn "Take a break!"
+    countDown (breakMinutes * 60)
+    pomodoro workMinutes breakMinutes
+
+main :: IO ()
+main = do
+    putStrLn "How many minutes would you like for work?"
+    workMinutes <- getLine
+    putStrLn "How many minutes would you like for break?"
+    breakMinutes <- getLine
+    let convertedWorkMinutes = read workMinutes :: Int
+    let convertedBreakMinutes = read breakMinutes :: Int
+    pomodoro convertedWorkMinutes convertedBreakMinutes
